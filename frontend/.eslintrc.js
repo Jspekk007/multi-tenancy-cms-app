d4e1a0083ee710d2c2cf4ab35333128be060b1d5
Johan Spekman <johanspekman@gmail.com>
2025-03-12 10:02:28 +0100

    FEAT: add pre-commit and linting

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'prettier/prettier': ['error', { semi: false, singleQuote: true }]
+    }
+}
