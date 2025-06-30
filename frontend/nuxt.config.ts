6b1874b2f687d7011be552e5e76251f6c9daa149
Johan Spekman <johan@macbookpro-1.home>
2025-06-30 20:41:35 +0200

    feat: add scss setup

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,8 +3,21 @@ export default defineNuxtConfig({
 
   modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
+  css: ['@/assets/css/main.css'],
   compatibilityDate: '2025-05-15',
 
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/scss/variables.scss" as *;
+            @use "@/assets/scss/mixins.scss" as *;
+          `,
+        },
+      },
+    },
+  },
   eslint: {
     config: {
       stylistic: true,
