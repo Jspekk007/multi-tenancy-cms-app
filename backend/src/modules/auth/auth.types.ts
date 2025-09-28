a95cc3ec16ae11beee5b4c714dd548a5b2e810e2
Johan Spekman <Johanspekman@gmail.com>
2025-09-28 11:24:59 +0200

    WIP: first auth setup module

--- backend/src/modules/auth/auth.types.ts
+++ backend/src/modules/auth/auth.types.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
 
 export const signupInputSchema = z.object({
-  name: z.string().min(2, "Name must be at least 2 characters long"),
-  domain: z.string().min(3, "Domain must be at least 3 characters long"),
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
+  name: z.string().min(2, 'Name must be at least 2 characters long'),
+  domain: z.string().min(3, 'Domain must be at least 3 characters long'),
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(8, 'Password must be at least 8 characters long'),
 });
 
 export interface LoginInput {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export type SignupInput = z.infer<typeof signupInputSchema>;
