bf4bcfca175a14dbf4fa6d6cd85130f7e2d16cf7
Johan Spekman <Johanspekman@gmail.com>
2025-09-30 16:00:33 +0200

    NEW: add login procedure

--- backend/src/modules/auth/auth.utils.ts
+++ backend/src/modules/auth/auth.utils.ts
@@ -1,16 +1,16 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-const SALT_ROUNDS = Number(process.env.BCRYPT_SALT_ROUNDS) || 10;
-const JWT_SECRET = process.env.JWT_SECRET;
+import { config } from '../../lib/config.js';
 
 export const hashPassword = async (password: string): Promise<string> =>
-  await bcrypt.hash(password, SALT_ROUNDS);
+  await bcrypt.hash(password, config.saltRounds);
 
 export const verifyPassword = async (password: string, hash: string): Promise<boolean> =>
   await bcrypt.compare(password, hash);
 
-export const generateToken = (payload: object): string =>
-  jwt.sign(payload, JWT_SECRET!, { expiresIn: '1h' });
+export const generateToken = (payload: object): string => {
+  return jwt.sign(payload, config.jwtSecret, { expiresIn: '1h' });
+};
 
-export const verifyToken = (token: string): string | object => jwt.verify(token, JWT_SECRET!);
+export const verifyToken = (token: string): string | object => jwt.verify(token, config.jwtSecret!);
